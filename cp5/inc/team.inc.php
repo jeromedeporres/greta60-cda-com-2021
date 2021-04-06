57c1cca97fbccf3ecdbc3aa56a01cabcf6b19455
daron-coder <lesly_lodin@msn.com>
2021-04-06 10:41:24 +0200

    PHP Day 1

---
+++ cp5/inc/team.inc.php
@@ -0,0 +1,40 @@
+<?php
+// Liste des membres de l'équipe
+$girls = array(
+    array(
+        'fname' => 'Shirley',
+        'female' => true,
+        'dob' => '2000-05-18',
+        'size' => 1.68
+    ),
+    array(
+        'fname' => 'Nathalie',
+        'female' => true,
+        'dob' => '2000-11-14',
+        'size' => 1.71
+    ),
+    array(
+        'fname' => 'Sara',
+        'female' => true,
+        'dob' => '2000-07-06',
+        'size' => 1.72
+    ),
+    array(
+        'fname' => 'Fatoumata',
+        'female' => true,
+        'dob' => '2000-03-19',
+        'size' => 1.78
+    ),
+    array(
+        'fname' => 'Sandrina',
+        'female' => true,
+        'dob' => '2000-10-26',
+        'size' => 1.8
+    ),
+    array(
+        'fname' => 'Odile',
+        'female' => true,
+        'dob' => '2000-08-15',
+        'size' => 1.81
+    )
+);
